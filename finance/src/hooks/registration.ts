05d00129780f1c5d0d964a312914b5392c2bfccb
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2024-09-16 11:05:05 -0700

    Update registration.ts

--- finance/src/hooks/registration.ts
+++ finance/src/hooks/registration.ts
@@ -11,4 +11,5 @@ export function initHooks(hooks: Hooks) {
     // Add hooks by calling hooks.register{ClientInit/BeforeCreateRequest/BeforeRequest/AfterSuccess/AfterError}Hook
     // with an instance of a hook that implements that specific Hook interface
     // Hooks are registered per SDK instance, and are valid for the lifetime of the SDK instance
+    hooks.registerBeforeRequestHook(new CustomHeaderHook());
 }
